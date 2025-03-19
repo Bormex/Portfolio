0312715a6ac32c41fdb15a2c25891ce91318f13c
Niclas Holzhey <niclas@MacBook-Pro-von-Niclas.local>
2025-03-19 12:25:08 +0100

    HTML - Constructe nearly done

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
--- src/app/main-content/portfolio-projects/portfolio-projects.component.ts
+++ src/app/main-content/portfolio-projects/portfolio-projects.component.ts
@@ -1,36 +1,41 @@
 import { Component } from '@angular/core';
 import { NgStyle } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { CommentSectionComponent } from "./comment-section/comment-section.component";
 
 @Component({
   selector: 'app-portfolio-projects',
   standalone: true,
-  imports: [NgStyle, CommonModule],
+  imports: [NgStyle, CommonModule, CommentSectionComponent],
   templateUrl: './portfolio-projects.component.html',
   styleUrl: './portfolio-projects.component.scss'
 })
 export class PortfolioProjectsComponent {
   Projects = [
-      {
-        "name": "Join",
-        "description": "Ein Projekt, das ich entwickelt habe, um Aufgabenverwaltung zu erleichtern.",
-        "languages": ["JavaScript", "TypeScript", "Angular", "HTML", "CSS"]
-      },
-      {
-        "name": "TaskMaster",
-        "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
-        "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"]
-      },
-      {
-        "name": "Engeenier",
-        "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
-        "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"]
-      },
-      {
-        "name": "Data-Scients",
-        "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
-        "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"]
-      }
+    {
+      "name": "TaskMaster",
+      "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
+      "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"],
+      "image": "./../../../assets/img/notebook_Pokedex.png"
+    },
+    {
+      "name": "Join",
+      "description": "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      "languages": ["JavaScript", "TypeScript", "Angular", "HTML", "CSS"],
+      "image": "./../../../assets/img/notebook_Join.png"
+    },
+    {
+      "name": "Engeenier",
+      "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
+      "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"],
+      "image": "./../../../assets/img/notebook_Pollo_loco.png"
+    },
+    {
+      "name": "Data-Scients",
+      "description": "Ein Projekt, das ich erstellt habe, um Teamarbeit und Projektmanagement zu verbessern.",
+      "languages": ["Python", "Django", "JavaScript", "Vue.js", "PostgreSQL"],
+      "image": "./../../../assets/img/notebook_Dabubble.png"
+    }
   ]
   
   
