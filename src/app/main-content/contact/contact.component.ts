29abc85fc7235e3bcb1c7d5da12374ac4093ce4b
Niclas Holzhey <niclas@MacBook-Pro-von-Niclas.local>
2025-03-27 16:19:07 +0100

    Inputfields validation done

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -8,35 +8,122 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [MatCheckboxModule, CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  inputReq: boolean = false;
-  inputOk: boolean = false;
+  inputNameOk: boolean = false;
+  inputEmailOk: boolean = false;
+  inputMessageOk: boolean = false;
+  checkboxOk: boolean = false;
 
+  checked: boolean = false;
   inputName: boolean = false;
   inputMail: boolean = false;
   inputMessage: boolean = false;
 
   activateBtn: boolean = false;
 
-  contactData = {
-    name: "",
-    email: "",
-    message: ""
-  }
+  validDomains: Array<string> = [
+    '.com',
+    '.net',
+    '.org',
+    '.info',
+    '.biz',
+    '.online',
+    '.shop',
+    '.de',
+    '.at',
+    '.ch',
+    '.fr',
+    '.it',
+    '.es',
+    '.nl',
+    '.uk',
+    '.us',
+    '.ca',
+    '.au',
+    '.jp',
+    '.cn',
+    '.ru',
+    '.br',
+    '.in',
+    '.io',
+    '.ai',
+    '.tech',
+    '.xyz',
+    '.dev',
+  ];
+  validMail = /[A-z]/g;
 
-  checkInput(para: boolean) {
-    console.log(para);
-    
-    if (this.contactData.name == " " || this.contactData.email == " " || this.contactData.message == " ") {
-      
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  checkInputfields(para: boolean) {
+    if (
+      this.inputNameOk &&
+      this.inputEmailOk &&
+      this.inputMessageOk &&
+      this.checked
+    ) {
+      this.activateBtn = true;
+    } else {
+      this.activateBtn = false;
     }
   }
 
   onSubmit() {
-    console.log(this.contactData);
-    console.log('Test');
-    
+    if (this.activateBtn) {
+      console.log(this.contactData);
+    }
+  }
+
+  checkValue(checkbox: any) {
+    this.checked = checkbox.checked;
+    if (checkbox == true) {
+      this.checkboxOk = true;
+    } else {
+      this.checkboxOk = false;
+    }
+    this.checkInputfields(this.checkboxOk);
+  }
+
+  checkNameInput(para: boolean): void {
+    if (this.contactData.name.length > 3) {
+      this.inputName = false;
+      this.inputNameOk = true;
+    } else {
+      this.inputNameOk = false;
+    }
+    this.checkInputfields(this.checkboxOk);
+  }
+
+  checkEmailInput(para: boolean): void {
+    if (
+      this.contactData.email.length > 3 &&
+      this.contactData.email.match(this.validMail) &&
+      this.contactData.email.includes('@') &&
+      this.validDomains.some((domains) =>
+        this.contactData.email.includes(domains)
+      )
+    ) {
+      this.inputMail = false;
+      this.inputEmailOk = true;
+    } else {
+      this.inputEmailOk = false;
+    }
+    this.checkInputfields(this.checkboxOk);
+  }
+
+  checkMsgInput(para: boolean): void {
+    if (this.contactData.message.length > 3) {
+      this.inputMessage = false;
+      this.inputMessageOk = true;
+    } else {
+      this.inputMessageOk = false;
+    }
+    this.checkInputfields(this.checkboxOk);
   }
 }
